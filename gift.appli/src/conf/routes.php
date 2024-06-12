8068079baf13a84f4cba46a26217861b3fc45847
despaqui9u <liamdespaqiis@gmail.com>
2024-06-12 10:47:00 +0200

    getPrestation

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 use gift\appli\app\actions\GetPrestationsAction;
 
+use gift\appli\app\actions\GetOnePrestationAction;
+
 return function(\Slim\App $app): \Slim\App {
 
     $app->get('/prestations', GetPrestationsAction::class)->setName('prestations');
+
+    $app->get('/prestation',GetOnePrestationAction::class)->setName('prestation');
 
     return $app;
 
